9b7229ed319efc3a6def2c636bdcb22976090527
sysadmin-as-a-service <35851895+sysadmin-as-a-service@users.noreply.github.com>
2021-06-05 23:40:17 +1200

    Adding dropdown and table rendering on dropdown change

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,17 +1,69 @@
 import React, { Component } from 'react';
-import { GetCustomers } from './GetCustomers'
+import Table from './Table';
+import { getCustomers, getOrdersByCustomerId } from '../store/storeutil'
 
 export class Home extends Component {
-  static displayName = Home.name;
+  constructor(props) {
+    super(props)
+
+    this.state = {
+       customers: [],
+       selectedCustomerId: '',
+       orders: [],
+       ordersLoading: true,
+       error: false
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.getCustomers = getCustomers.bind(this)
+    this.getOrdersByCustomerId = getOrdersByCustomerId.bind(this)
+
+  }
   
+  static displayName = Home.name;
+
+  
+  componentDidMount() {
+
+    this.getCustomers()
+
+  }
+  
+  handleChange(event) {
+    
+    this.setState({selectedCustomerId: event.target.value})
+    this.setState({orders: this.getOrdersByCustomerId(event.target.value)})
+
+  }
+
   render () {
+
     return (
       <div>
+
+
         <h1>Please Select a Customer</h1>
-        <GetCustomers />
-        
+
+        <select onChange={this.handleChange}>
+
+          {this.state.customers.map((customer, index) => (
+            <option key={index} value={customer.id}>{customer.name}</option>
+          )) }
+
+        </select>
+
+        {
+          this.state.selectedCustomerId != '' && !this.state.ordersLoading && !this.state.error ?
+          <Table contents={this.state.orders} /> :
+          <div>Loading orders...</div>
+        }
 
       </div>
     );
   }
+
+  
+
 }
+
+
