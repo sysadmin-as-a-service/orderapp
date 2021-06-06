b9cfa7729e64cbe64f8ceb9245e0f4fee4be8ca5
sysadmin-as-a-service <35851895+sysadmin-as-a-service@users.noreply.github.com>
2021-06-06 22:41:06 +1200

    Removing order from orderitems

--- Models/OrderItem.cs
+++ Models/OrderItem.cs
@@ -6,7 +6,6 @@ namespace orderapp.Models
     public class OrderItem
     {
         public int Id {get; set; }
-        public Order Order { get; set; }
         public Product Product {get; set;}
         public int Quantity {get; set;}
 
