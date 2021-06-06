88d90b0600a16b478bf44037536d353ed744a39c
sysadmin-as-a-service <35851895+sysadmin-as-a-service@users.noreply.github.com>
2021-06-06 22:04:09 +1200

    Include List of OrderItems

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -31,9 +31,50 @@ namespace orderapp.Controllers
 
             using (IDbConnection db = new SqlConnection(_connectionString))
             {
-                List<Order> Orders = (await db.QueryAsync<Order>("sp_GetOrdersByCustomerId" , param: new {customerId}, commandType: CommandType.StoredProcedure)).ToList();
-                return Ok(Orders);
+                string sql = @"
+                    SELECT *
+                    FROM orders
+                    LEFT JOIN orderitems ON orders.id=orderitems.orderid
+                    WHERE orders.customerId = @CustomerId
+                    ";
+
+                var parameters = new { CustomerId = customerId };
+
+                // need to create this outside the lambda
+                var orderDictionary = new Dictionary<int, Order>();
+
+                var data = db.Query<Order, OrderItem, Order>(
+                    sql,
+                    (order, orderItem) => 
+                    { 
+                        // create a new dummy obj
+                        Order orderEntry;
+
+                        // check if we've already added this order ID to our orderDictionary
+                        // if we haven't, add it and make orderItems a new empty list
+                        // then make orderEntry the matching order from our dictionary
+                        if(!orderDictionary.TryGetValue(order.Id, out orderEntry))
+                        {
+                            orderEntry = order;
+                            orderEntry.OrderItems = new List<OrderItem>();
+                            orderDictionary.Add(orderEntry.Id, orderEntry);
+                        }
+
+                        // add orderItem to the list of orderItems for 
+                        orderEntry.OrderItems.Add(orderItem);
+                        return orderEntry;
+                    }
+                ).Distinct().ToList();
+
+
+                return Ok(data);
             }
+
+            // using (IDbConnection db = new SqlConnection(_connectionString))
+            // {
+            //     List<Order> Orders = (await db.QueryAsync<Order>("sp_GetOrdersByCustomerId" , param: new {customerId}, commandType: CommandType.StoredProcedure)).ToList();
+            //     return Ok(Orders);
+            // }
             
         }
 
