80aee7678b891aa72efdfa7b1b1cc5b4aa6ce03c
sysadmin-as-a-service <35851895+sysadmin-as-a-service@users.noreply.github.com>
2021-06-06 15:05:36 +1200

    Make table display dates & numbers pretty

--- ClientApp/src/components/Table.js
+++ ClientApp/src/components/Table.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import moment from 'moment'
 
 export default class Table extends Component {
     constructor(props) {
@@ -12,33 +13,55 @@ export default class Table extends Component {
     
 
     render() {
+
+        function formatTableData(value) {
+
+            // using moment here because manual options are error prone!
+            // table will show matching dates in a friendly date format
+            if(moment(value, [moment.HTML5_FMT.DATETIME_LOCAL_SECONDS , "YYYY-MM-DD"], true).isValid()){
+                var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+                return new Date(value).toLocaleDateString("en-US", options)
+            }
+
+            // table will display max 2 digits
+            if(typeof(value) === 'number'){
+                return Math.round(value * 100) / 100
+            }
+
+            return value
+
+        }
+
+        let tableHeadersHTML = this.state.tableHeaders.map((header, index) => (
+            <th key={index}>{header}</th>
+        ))
+
+        let tableRowsHTML = this.state.tableContents.map((row, index) => (
+            <tr key={index}>
+                {
+                    Object.values(row).map((value, index) => 
+                    (
+                        <td key={index}>
+                            {formatTableData(value)}
+                        </td>
+                    ))
+                }
+            </tr>
+        ))
+
+
+
         return (
             <div>
 
                 <table>
                     <thead>
                         <tr>
-                        {
-                            this.state.tableHeaders.map((header, index) => (
-                                <th key={index}>{header}</th>
-                            ))
-                        }
+                            {tableHeadersHTML}
                         </tr>
                     </thead>
                     <tbody>
-                        {
-                            this.state.tableContents.map((row, index) => (
-                                <tr key={index}>
-                                    {
-                                        Object.values(row).map((value, index) => (
-                                            <td key={index}>
-                                                {value}
-                                            </td>
-                                        ))
-                                    }
-                                </tr>
-                            ))
-                        }
+                        {tableRowsHTML}
                     </tbody>
                 </table>
             </div>
